0d1d693f1f7569297ae58d2c128446e19f3f06ab
Charlie <ruggedcafe@gmail.com>
2021-12-19 17:12:13 +0900

    feat: Card 클래스 구현

--- src/main/java/blackjack/model/Card.java
+++ src/main/java/blackjack/model/Card.java
@@ -5,16 +5,25 @@ public class Card {
     public static final String HEART = "하트";
     public static final String DIAMOND = "다이아몬드";
     public static final String CLOVER = "클로버";
+    public static final int MIN_NUM_RANGE = 2;
+    public static final int MAX_NUM_RANGE = 10;
 
-    private String number;
+    private int number;
     private String suit;
 
-    public Card(String number, String suit) {
+    public Card(int number, String suit) {
+        checkNumRange(number);
         this.number = number;
         this.suit = suit;
     }
 
-    public String getNumber() {
+    private void checkNumRange(int number) {
+        if (number > MAX_NUM_RANGE || number < MIN_NUM_RANGE) {
+            throw new IllegalArgumentException("카드 숫자는 2 ~ 10 사이의 숫자만 가능합니다.");
+        }
+    }
+
+    public int getNumber() {
         return number;
     }
 
@@ -22,7 +31,7 @@ public class Card {
         return suit;
     }
 
-    public String getCard(String number, String suit) {
+    public String getCard() {
         return number + suit;
     }
 }
--- src/test/java/blackjack/model/CardTest.java
+++ src/test/java/blackjack/model/CardTest.java
@@ -3,12 +3,25 @@ package blackjack.model;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CardTest {
 
     @Test
     void 카드_생성_테스트() {
-        Card card = new Card("10", Card.SPADE);
-        assertThat(card.getCard(card.getNumber(), card.getSuit())).isEqualTo("10스페이드");
+        Card card1 = new Card(10, Card.SPADE);
+        assertThat(card1.getCard()).isEqualTo("10스페이드");
+        Card card2 = new Card(2, Card.HEART);
+        assertThat(card2.getCard()).isEqualTo("2하트");
+        Card card3 = new Card(7, Card.CLOVER);
+        assertThat(card3.getCard()).isEqualTo("7클로버");
+    }
+
+    @Test
+    void 카드_생성_숫자_범위_테스트() {
+        assertThatThrownBy(() -> new Card(11, Card.CLOVER))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Card(1, Card.CLOVER))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
