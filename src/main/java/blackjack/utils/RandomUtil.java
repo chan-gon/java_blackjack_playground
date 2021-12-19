dee3a7acf899936dd19654efd9868df1e2e8465d
Charlie <ruggedcafe@gmail.com>
2021-12-19 22:59:43 +0900

    refactor: RandomUtil 클래스 수정

--- src/main/java/blackjack/utils/RandomUtil.java
+++ src/main/java/blackjack/utils/RandomUtil.java
@@ -3,12 +3,11 @@ package blackjack.utils;
 import java.util.Random;
 
 public class RandomUtil {
+    public static final int MIN_NUM = 2;
+    public static final int MAX_NUM = 10;
 
     public static int generateRandomNum() {
         Random random = new Random();
-        int min = 2;
-        int max = 10;
-        return random.nextInt(max - min) + min;
-
+        return random.nextInt(MAX_NUM - MIN_NUM) + MIN_NUM;
     }
 }
